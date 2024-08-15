54d59bc453a775dd1c68468b03bb2ae276494638
amira alrshidi <ameerah.alrashidi0111@gmail.com>
2024-08-15 14:20:16 +0300

    add parent and child models

--- NurseriesGuide/parents/views.py
+++ NurseriesGuide/parents/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.http import HttpRequest, HttpResponse, Http404
+from django.core.paginator import Paginator
+from django.contrib import messages
+from django.db import IntegrityError
 
-# Create your views here.
+
+def add_child(request:HttpRequest):
+    pass
+
+def update_parent(request:HttpRequest):
+    pass
+def delete_child(request:HttpRequest):
+    pass
+
+def update_child(request:HttpRequest):
+    pass
+
