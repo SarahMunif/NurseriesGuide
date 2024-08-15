0c963a6283906883cfa04c68128d013e7a87a79b
raynasser <rnff-333@hotmail.com>
2024-08-15 14:16:18 +0300

    registrations models and main views

---
+++ NurseriesGuide/registrations/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name = "registrations"
+
+
+
+urlpatterns = [
+    path('create/', views.registration_create, name='registration_create'),
+    path('', views.registration_list, name='registration_list'),
+    path('<int:pk>/', views.registration_detail, name='registration_detail'),
+    path('<int:pk>/update-status/', views.registration_update_status, name='registration_update_status'),
+]
